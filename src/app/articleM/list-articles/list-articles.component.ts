a99c7e7fc0c4c7682d236556aa221eaef92e60bd
Vinod Sakhare <68527908+vinodsakhare@users.noreply.github.com>
2023-03-27 03:38:35 +0530

    Feature/vinod (#6)
    * add article form in popup

--- src/app/articleM/list-articles/list-articles.component.ts
+++ src/app/articleM/list-articles/list-articles.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-list-articles',
   templateUrl: './list-articles.component.html',
   styleUrls: ['./list-articles.component.css']
 })
-export class ListArticlesComponent {
+export class ListArticlesComponent implements OnInit{
 
+  constructor(private _articleService: ArticleService){}
+
+  displayedColumns: string[] = [
+    'id',
+    'productType',
+    'brandName',
+    'mrp',
+    'mfgDate',
+    'articleNo',
+    'color',
+    'size',
+    'quantity'
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit(): void {
+      this.getArticles();
+  }
+
+  getArticles() {
+    this._articleService.getArticle().subscribe({
+      next : (res :any) =>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err : any) => {
+        console.log(err);
+      }
+    })
+  }
 }
--- src/app/sharedM/shared.module.ts
+++ src/app/sharedM/shared.module.ts
@@ -1,17 +1,34 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatFormFieldModule} from "@angular/material/form-field";
 import { MatCardModule } from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+
+import { HttpClientModule } from  '@angular/common/http';
+
+import {MatTableModule} from '@angular/material/table';
+
 @NgModule(
   {
     declarations: [],
     imports: [FormsModule,
-      CommonModule,ReactiveFormsModule,MatFormFieldModule,MatCardModule,MatToolbarModule,MatButtonModule],
+      CommonModule,ReactiveFormsModule,MatFormFieldModule,
+      MatCardModule,MatToolbarModule,MatButtonModule,MatDialogModule,MatSelectModule,
+      MatOptionModule,MatInputModule,MatNativeDateModule,MatDatepickerModule,MatDialogModule,HttpClientModule,
+      MatTableModule],
     exports: [FormsModule,
-      CommonModule,ReactiveFormsModule,MatFormFieldModule,MatCardModule,MatToolbarModule,MatButtonModule],
+      CommonModule,ReactiveFormsModule,MatFormFieldModule,
+      MatCardModule,MatToolbarModule,MatButtonModule,MatDialogModule,MatSelectModule,
+      MatOptionModule,MatInputModule,MatNativeDateModule,MatDatepickerModule,MatDialogModule,HttpClientModule,
+      MatTableModule],
     providers: []
   }
 )
