fdbdea206e2471b271a36c595a9f42bd8fb6366c
vinod sakhare <vinodsakhare73@gmail.com>
2023-03-27 03:21:49 +0530

    add article form in popup

--- src/app/articleM/create-article/create-article.component.ts
+++ src/app/articleM/create-article/create-article.component.ts
@@ -1,5 +1,8 @@
+import { DialogRef } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-create-article',
@@ -12,7 +15,8 @@ export class CreateArticleComponent {
 
   productTypeList: any = ['shirt','pant'];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private _articleService: ArticleService, 
+    private _dialogRef: DialogRef<CreateArticleComponent>, private _router: Router) {
     this.articleForm = this.formBuilder.group({
       productType: ['', Validators.required],
       brandName: ['', Validators.required],
@@ -29,5 +33,15 @@ export class CreateArticleComponent {
 
   onSubmit() {
     console.log(this.articleForm.value);
+    this._articleService.addArticle(this.articleForm.value).subscribe({
+      next: (val:any) => {
+        alert('Article added successfully');
+        this._dialogRef.close();
+        this._router.navigate(['list']);
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
   }
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateArticleComponent } from 'src/app/articleM/create-article/create-article.component';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(private _dialog: MatDialog) {}
+
+  onCreateArticleClick() {
+    this._dialog.open(CreateArticleComponent);
+  }
 
 }
